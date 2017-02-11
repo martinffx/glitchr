a132915408277fd1316bbe9d76b979745ed1a379
Martin C. Richards <martin@travelstart.co.za>
2017-02-11 14:19:19 +0200

    repeatable build fuckery

--- circle_pre_build.sh
+++ circle_pre_build.sh
@@ -13,16 +13,9 @@ if ! asdf | grep version; then
     asdf plugin-add elixir https://github.com/HashNuke/asdf-elixir.git
 fi
 
-# Extract vars from elixir_buildpack.config
-. elixir_buildpack.config
-
-# Write .tool-versions
-echo “erlang $erlang_version” >> .tool-versions
-echo “elixir $elixir_version” >> .tool-versions
-
 # Install erlang/elixir
-asdf install erlang $erlang_version
-asdf install elixir $elixir_version
+asdf install erlang
+asdf install elixir
 
 # Get dependencies
 yes | mix deps.get
