2c40502aabb0c491bcd80472d6f3ccfd5d6b940e
Martin C. Richards <martin@travelstart.co.za>
2017-02-12 17:20:20 +0200

    build 5

---
+++ circle_post_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+# Build
+./node_modules/brunch/bin/brunch b -p && MIX_ENV=prod mix do phoenix.digest, release --env=prod
+
+# Dockerise
+docker build -t martinffx/glitchr .
+
+# Exit successfully
+exit 0
--- circle_pre_build.sh
+++ circle_pre_build.sh
@@ -24,12 +24,5 @@ asdf install elixir $elixir_version
 yes | mix deps.get
 yes | mix compile
 
-# Build
-npm install
-./node_modules/brunch/bin/brunch b -p && MIX_ENV=prod mix do phoenix.digest, release --env=prod
-
-# Dockerise
-docker build -t martinffx/glitchr .
-
 # Exit successfully
 exit 0
