7b288f99ad7e2803b9dee547d5d2eaee9c97d070
Martin C. Richards <martin@travelstart.co.za>
2017-02-21 19:40:23 +0200

    migrate task

--- rel/tasks.exs
+++
@@ -1,9 +0,0 @@
-defmodule Release.Tasks do
-  def migrate do
-    {:ok, _} = Application.ensure_all_started(:my_app)
-
-    path = Application.app_dir(:my_app, "priv/repo/migrations")
-
-    Ecto.Migrator.run(Glitchr.Repo, path, :up, all: true)
-  end
-end
